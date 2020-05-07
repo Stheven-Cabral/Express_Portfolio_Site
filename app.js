4e18f7387ea703dde6ae7320410696066e88ba41
Stheven Cabral <stheven.cabral@gmail.com>
2020-05-07 10:54:03 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const { projects } = require('./data.json');
+const data = require('./data.json');
 
 const app = express()
 
 app.set('view engine', 'pug');
 
+/*Serve static files*/
+app.use(express.static('public'));
+
 /*Route to the home page*/
 app.get('/', (req, res, next) => {
-    res.send('<h1>Hello</h1>');
+    console.log(data.projects);
+    res.render('index', {projects: data.projects});
 });
 
-app.get('/about', (req, res, next) => {});
+app.get('/about', (req, res, next) => {
+    res.render('about');
+});
 
-app.get('/project', (req, res, next) => {});
-
+app.get('/project:id', (req, res, next) => {
+    res.send('<h1>work in progress</h1>');
+});
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
