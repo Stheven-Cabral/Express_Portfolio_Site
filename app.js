a427e68abc72999156dbfc060f90e26561a2df14
Stheven Cabral <stheven.cabral@gmail.com>
2020-05-07 05:47:48 -0700

    05072020 1747

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const express = require('express');
+const { projects } = require('./data.json');
 
 const app = express()
+
+app.set('view engine', 'pug');
+
+/*Route to the home page*/
+app.get('/', (req, res, next) => {
+    res.send('<h1>Hello</h1>');
+});
+
+app.get('/about', (req, res, next) => {});
+
+app.get('/project', (req, res, next) => {});
+
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
