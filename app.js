758cd1136090cbcc36320b3042140732ec665c50
Stheven Cabral <stheven.cabral@gmail.com>
2020-05-10 01:24:26 -0700

    Finished app.js comments.

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 /***
- * 
+ * The following require methods load the 'express' framework and the 'data.json' file.
+ * The express() function is called to create a express application for my portfolio site.
  */
 const express = require('express');
 const data = require('./data.json');
-
 const app = express();
 
-/*Pug setup*/
+
+/***
+ * Set the template engine to pug to view template files as HTML.
+ */
 app.set('view engine', 'pug');
 
-/*Serve static files*/
+
+/***
+ * The following code serves static assets from the 'public' directory.
+ */
 app.use(express.static('public'));
 
-/*Route to the home page*/
+
+/***
+ * Defined a route to the homepage that renders the 'index' template.
+ */
 app.get('/', (req, res, next) => {
     res.render('index', {projects: data.projects});
 });
 
-/*Route to the about page*/
+
+/***
+ * Defined a route to the about page that renders the 'about' template.
+ */
 app.get('/about', (req, res, next) => {
     res.render('about');
 });
 
-/*Route to the about page*/
+
+/***
+ * Defined a route to specific project pages.
+ * If a valid project page is requested, the 'project' template is rendered; else, a new error is defined.
+ */
 app.get('/project/:id', (req, res, next) => {
     const projectId = req.params.id;
     const project = data.projects.find( ({ id }) => id === +projectId );
@@ -37,7 +53,11 @@ app.get('/project/:id', (req, res, next) => {
     } 
 });
 
-/*Catches any 404 errors*/
+
+/***
+ * The following middleware is executed when no previous routes are executed.
+ * The middleware functions creates a new error 'err' and passes it to the next() function.
+ */
 app.use((req, res, next) => {
     const err = new Error("I'm sorry. Page Not Found");
     console.log("We're sorry. Something went wrong.");
@@ -45,7 +65,11 @@ app.use((req, res, next) => {
     next(err);
 });
 
-/*Error handling*/
+
+/***
+ * The following error middleware handles any errors passed to it and renders the 'error' template.
+ * local 'error' variable is defined in order to set the response status to the err.status.
+ */
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
